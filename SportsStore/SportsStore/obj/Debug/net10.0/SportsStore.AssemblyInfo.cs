ce6dc7af8da5b9735f5041207b4a14a43233056d
Chroma Dev <Chroma Dev@CHROMADEV>
2025-12-24 10:26:56 +0100

    Application folders created.

--- SportsStore/SportsStore/obj/Debug/net10.0/SportsStore.AssemblyInfo.cs
+++ SportsStore/SportsStore/obj/Debug/net10.0/SportsStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1797bcbd227728a499857b0fe7fb621f334cf127")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b55655ac05601fedd387c340f0c9b30db2c6d91")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
